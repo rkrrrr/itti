727e615319be93efb44a30f45f2f6611d8341d77
Ricardo Samudio <ricardo@teki.com.py>
2023-07-27 18:48:49 -0400

    cambios

--- components/CrearTodo.tsx
+++ components/CrearTodo.tsx
@@ -1,22 +1,43 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-function CrearTodo() {
+interface input {
+  title: string;
+  completed: boolean;
+}
+
+function CrearTodo({input ,setInputs, createTodo}: { createTodo:()=> void, input: input, setInputs: React.Dispatch<React.SetStateAction<input>>}) {
+  
   return (
     <div className=''>
     <Form>
       <Form.Group className="mb-3 p-3" controlId="">
-        <Form.Label>Titulo de la tarea</Form.Label>
-        <Form.Control type="text" placeholder="Introduce el titulo de la tarea"/>
+        <Form.Label >Titulo de la tarea</Form.Label>
+        <Form.Control onChange={(e)=>{
+          setInputs({
+            ...input,
+            title: e.target.value
+          })
+        }} type="text" placeholder="Introduce el titulo de la tarea"/>
       </Form.Group>
         <div className="mb-3 p-3">
         <p>Titulo de la tarea</p>
-            <select name="estado" id="estadoTarea">
-                <option value="1">Terminada</option>
+            <select onChange={(e)=>{
+              setInputs({
+                ...input,
+                completed: e.target.value === '1' ? true : false
+              })
+            }} name="estado" id="estadoTarea">
                 <option value="2">No terminada</option>
+                <option value="1">Terminada</option>
             </select>
         </div>
-      <Button variant="primary" type="submit">
+      <Button 
+      className='m-auto d-block p-3 '
+      onClick={(e)=>{
+        e.preventDefault()
+        createTodo()
+      }} variant="primary" type="submit">
         Submit
       </Button>
     </Form>
--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,20 @@ interface todo {
   completed: boolean;
 }
 
+interface input {
+  title: string;
+  completed: boolean;
+}
+
 function App() {
   const [todos, setTodos] = useState<todo[]>([]);
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState<input>({
+    title: "",
+    completed: false,
+  });
+
+  const todosReverse = todos.reverse();
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos")
@@ -20,34 +32,76 @@ function App() {
       .then((json) => setTodos(json));
   }, []);
 
+  const createTodo = () =>{
+    fetch('https://jsonplaceholder.typicode.com/todos',{
+      method: 'POST',
+      body: JSON.stringify({
+        title: input.title,
+        completed: input.completed
+        }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+    .then((response) => response.json())
+    .then((json) =>{
+      setTodos([
+        ...todos,
+        json
+      ])
+    }
+    )
+  }
+
+
   return (
     <>
       <Navbar />
       <div className="container">
         <h2 className="text-center">Tareas por terminar</h2>
-        <div className="w-75 d-flex-inline m-auto bg-success">
-          <h4>Crear tarea</h4>
-          <CrearTodo />
+        <div className="w-75 d-flex-inline m-auto border-dark border">
+          <h4
+                 onClick={() => setOpen(!open)}
+                 aria-controls="example-collapse-text"
+                 aria-expanded={open} 
+            >Crear tarea</h4>
+          <Collapse in={open}>
+            <div id="example-collapse-text">
+            <CrearTodo
+              input={input}
+              setInputs={setInput}
+              createTodo={createTodo}
+            />
+            </div>
+          </Collapse>
+          
         </div>
-        {todos.map((todo) => (
+        <div className="d-flex gap-2 mt-2 flex-column-reverse">
+
+        {todosReverse.map((todo) => (
           <div
-            className={
-              todo.completed
-                ? "d-flex w-75 justify-content-between m-auto align-items-center border border-primary p-2"
-                : "d-flex w-75 justify-content-between m-auto align-items-center border border-primary p-2 bg-warning"
-            }
-            key={todo.id}
+          className={
+            todo.completed
+            ? "d-flex w-75 justify-content-between m-auto align-items-center rounded p-2  bg-success "
+            : "d-flex w-75 justify-content-between m-auto align-items-center rounded p-2 bg-warning "
+          }
+          key={todo.id}
           >
             <div className="w-75">
               <h3>{todo.title}</h3>
               <p>{todo.completed ? "Completed" : "Not Completed"}</p>
             </div>
-            <div>
+            <div className="d-flex flex-nowrap">
+              {
+                todo.completed?
               <Button className="bg-primary m-1">Terminar</Button>
+              : <></>
+              }
               <Button className="bg-danger m-1">Eliminar</Button>
             </div>
           </div>
         ))}
+        </div>
       </div>
     </>
   );
