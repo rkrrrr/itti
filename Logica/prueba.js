70d85214e0d4b95eba98d95ba4caf5356bc660d0
Ricardo Samudio <ricardo@teki.com.py>
2023-07-28 15:25:11 -0400

    adjunto prueba de logica

---
+++ Logica/prueba.js
@@ -0,0 +1,12 @@
+let x = 2
+let y = 3
+
+console.log(`El valor inicial de X es ${x}`)
+console.log(`El valor inicial de Y es ${y}`)
+x =  x + y
+y = x - y
+x = x - y
+
+console.log(` el valor final de X es ${x}`)
+console.log(` el valor final de Y es ${y}`)
+
